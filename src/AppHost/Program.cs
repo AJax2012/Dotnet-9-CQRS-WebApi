5d64bf0799040dbfeb1021f62c6257b0eff37590
ajax <adga2012@gmail.com>
2025-04-08 10:36:33 -0600

    remove SourceName from AppHost references

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -1,22 +1,22 @@
 using SourceName.AppHost;
 
 var builder = DistributedApplication.CreateBuilder(args);
-var toDosName = "SourceNameToDos";
+const string pgName = "SourceName";
 
-var postgres = builder.AddPostgres("SourceNamePostgres")
-    .WithEnvironment("POSTGRES_DB", toDosName)
+var postgres = builder.AddPostgres("postgres")
+    .WithEnvironment("POSTGRES_DB", pgName)
     .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent)
     .WithPgAdmin();
     
-var postgresDatabase = postgres.AddDatabase(toDosName);
+var postgresDatabase = postgres.AddDatabase(pgName);
 
-var migrator = builder.AddProject<Projects.SourceName_Migrator>("SourceNameMigrator")
+var migrator = builder.AddProject<Projects.SourceName_Migrator>("migrator")
     .WithReference(postgresDatabase)
     .WaitFor(postgresDatabase)
     .WithEnvironment("ConnectionStrings:Default", postgresDatabase.Resource.ConnectionStringExpression);
 
-builder.AddProject<Projects.SourceName_Api>("SourceNameApi")
+builder.AddProject<Projects.SourceName_Api>("api")
     .WithReference(postgresDatabase)
     .WaitFor(postgresDatabase)
     .WaitForCompletion(migrator)
