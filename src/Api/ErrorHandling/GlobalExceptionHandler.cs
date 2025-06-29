3c9660d0d72d2a73890bbacbab153788d26d2493
ajax <adga2012@gmail.com>
2025-06-29 09:56:23 -0600

    Create GlobalExceptionHandler

---
+++ src/Api/ErrorHandling/GlobalExceptionHandler.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+using ILogger = Serilog.ILogger;
+
+namespace SourceName.Api.ErrorHandling;
+
+internal sealed class GlobalExceptionHandler(ILogger logger, IWebHostEnvironment environment) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        logger.Error(exception, "Unhandled exception occured");
+        
+        var problemDetails = new ProblemDetails
+        {
+            Title = "An error occured",
+            Detail = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Type = "https://tools.ietf.org/html/rfc9110#name-500-internal-server-error",
+            Extensions =
+            {
+                { "traceId", httpContext.TraceIdentifier }
+            }
+        };
+
+        if (environment.IsDevelopment())
+        {
+            problemDetails.Extensions.Add("exception", exception.Message);
+            problemDetails.Extensions.Add("stackTrace", exception.StackTrace);
+
+            if (exception.InnerException != null)
+            {
+                problemDetails.Extensions.Add("innerException", exception.InnerException.Message);
+                problemDetails.Extensions.Add("innerStackTrace", exception.InnerException.StackTrace);
+            }
+        }
+        
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+        httpContext.Response.ContentType = "application/problem+json";
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
